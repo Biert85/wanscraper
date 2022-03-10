d723c2be6d535e94cdc4c3a395a04976daaf8284
Geert Lamers <geert.lamers@pidz.nl>
2022-03-10 22:49:56 +0100

    Remove FeedIO

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,20 +2,11 @@
 
 namespace App\Providers;
 
-use App\FeedIo\Adapter\HttpAdapter;
-use FeedIo\FeedIo;
 use Illuminate\Support\ServiceProvider;
-use Psr\Log\LoggerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(FeedIo::class, function($app): FeedIo {
-            $logger = $app->make(LoggerInterface::class);
-            $client = new HttpAdapter();
-
-            return new FeedIo($client, $logger);
-        });
     }
 }
